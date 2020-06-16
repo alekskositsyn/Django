0cefcb72aa53b8e3383253ab7bfff2d0777eea89
aleks.kositsyn <aleks.kositsyn@gmail.com>
2020-06-16 18:41:17 +0300

    start HM2

--- mixer/mainapp/views.py
+++ mixer/mainapp/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
+
 def home(request):
     return render(request, 'mainapp/home.html')
+
+
 def catalog(request):
     return render(request, 'mainapp/catalog.html')
+
+
 def contacts(request):
     return render(request, 'mainapp/contacts.html')
+
+
 def product(request):
     return render(request, 'mainapp/product.html')
