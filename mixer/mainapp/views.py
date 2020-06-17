d17a911d854926ebdfd20b72be535441e9659671
aleks.kositsyn <aleks.kositsyn@gmail.com>
2020-06-17 15:07:17 +0300

    add base.html шаблон

--- mixer/mainapp/views.py
+++ mixer/mainapp/views.py
@@ -2,16 +2,28 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'mainapp/home.html')
+    context = {
+        'page_title': 'главная'
+    }
+    return render(request, 'mainapp/home.html', context)
 
 
 def catalog(request):
-    return render(request, 'mainapp/catalog.html')
+    context = {
+        'page_title': 'каталог'
+    }
+    return render(request, 'mainapp/catalog.html', context)
 
 
 def contacts(request):
-    return render(request, 'mainapp/contacts.html')
+    context = {
+        'page_title': 'контакты'
+    }
+    return render(request, 'mainapp/contacts.html', context)
 
 
 def product(request):
-    return render(request, 'mainapp/product.html')
+    context = {
+        'page_title': 'продукт'
+    }
+    return render(request, 'mainapp/product.html', context)
