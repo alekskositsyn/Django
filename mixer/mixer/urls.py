8f04e495dcad94f7f12b0fd90eacc361367715c8
aleks.kositsyn <aleks.kositsyn@gmail.com>
2020-06-20 19:15:03 +0300

    add 'image' to DB for products

--- mixer/mixer/urls.py
+++ mixer/mixer/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -5,3 +7,6 @@ urlpatterns = [
     path('', include('mainapp.urls', namespace='main')),
     path('admin/', admin.site.urls, name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
