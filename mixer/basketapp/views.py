5759b7157635ad577d0738e5310846da5a4c69e7
aleks.kositsyn <aleks.kositsyn@gmail.com>
2020-06-28 21:13:49 +0300

    add basketapp

---
+++ mixer/basketapp/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+
+from basketapp.models import Basket
+from mainapp.models import Product
+
+
+def add_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    basket = Basket.objects.filter(user=request.user, product=product).first()
+    # basket = request.user.basket_set.filter(product=pk).first()
+
+    if not basket:
+        basket = Basket(user=request.user, product=product)
+
+    basket.quantity += 1
+    basket.save()
+
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
--- mixer/mixer/urls.py
+++ mixer/mixer/urls.py
@@ -6,6 +6,7 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('mainapp.urls', namespace='main')),
     path('auth/', include('authapp.urls', namespace='auth')),
+    path('basket/', include('basketapp.urls', namespace='basket')),
 
     path('admin/', admin.site.urls, name='home'),
 ]
