eaace48d9bbd811bd4af4666f92d719712073498
aleks <aleks.kositsyn@gmail.com>
2020-08-06 17:52:50 +0300

    lesson7 -LOW CACHE in mainapp views

--- mixer/mainapp/views.py
+++ mixer/mainapp/views.py
@@ -4,16 +4,90 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from mainapp.models import ProductCategory, Product
+from django.core.cache import cache
 
+from mixer.settings import LOW_CACHE
+
+
+# from doc
+def get_links_menu():
+    if LOW_CACHE:
+        key = 'links_menu'
+        links_menu = cache.get(key)
+        if links_menu is None:
+            links_menu = ProductCategory.objects.filter(is_active=True)
+            cache.set(key, links_menu)
+        return links_menu
+    else:
+        return ProductCategory.objects.filter(is_active=True)
+
+
+def get_category(pk):
+    if LOW_CACHE:
+        key = f'product_category_{pk}'
+        category = cache.get(key)
+        if category is None:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            cache.set(key, category)
+        return category
+    else:
+        return get_object_or_404(ProductCategory, pk=pk)
+
+
+def get_products():
+    if LOW_CACHE:
+        key = 'products'
+        products = cache.get(key)
+        if products is None:
+            products = Product.objects.filter(is_active=True,
+                                              category__is_active=True
+                                              ).select_related('category')
+            cache.set(key, products)
+        return products
+    else:
+        return Product.objects.filter(is_active=True,
+                                      category__is_active=True
+                                      ).select_related('category')
+
+
+def get_product(pk):
+    if LOW_CACHE:
+        key = f'product_{pk}'
+        product = cache.get(key)
+        if product is None:
+            product = get_object_or_404(Product, pk=pk)
+            cache.set(key, product)
+        return product
+    else:
+        return get_object_or_404(Product, pk=pk)
+
+
+def get_products_in_product_category(pk):
+    if LOW_CACHE:
+        key = f'products_in_product_category{pk}'
+        products = cache.get(key)
+        if products is None:
+            products = Product.objects.filter(
+                category__pk=pk,
+                is_active=True,
+                category__is_active=True
+            )
+            cache.set(key, products)
+        return products
+    else:
+        return Product.objects.filter(
+            category__pk=pk,
+            is_active=True,
+            category__is_active=True
+        )
+
+
+# local
 
 def get_hot_product():
     hot_product_pk = random.choice(Product.objects.values_list('pk', flat=True))
-    hot_product = Product.objects.get(pk=hot_product_pk)
+    hot_product = get_product(hot_product_pk)
     return hot_product
-
-
-def get_menu():
-    return ProductCategory.objects.filter(is_active=True)
 
 
 def home(request):
@@ -28,7 +102,7 @@ def catalog(request):
 
     context = {
         'page_title': 'каталог',
-        'products_categories': get_menu(),
+        'products_categories': get_links_menu(),
         'products': products,
     }
     return render(request, 'mainapp/catalog.html', context)
@@ -37,11 +111,11 @@ def catalog(request):
 def category_products(request, pk, page=1):
     if pk == '0':
         category = {'pk': 0, 'name': 'все'}
-        products = Product.objects.filter(is_active=True)
+        products = get_products()
     else:
-        category = get_object_or_404(ProductCategory, pk=pk)
-        products = category.product_set.all()
-    products_paginator = Paginator(products, 3)
+        category = get_category(pk)
+        products = get_products_in_product_category(pk)
+    products_paginator = Paginator(products, 6)
     try:
         products = products_paginator.page(page)
     except PageNotAnInteger:
@@ -51,7 +125,7 @@ def category_products(request, pk, page=1):
 
     context = {
         'page_title': 'продукты по категории',
-        'products_categories': get_menu(),
+        'products_categories': get_links_menu(),
         'products': products,
         'category': category,
     }
@@ -66,13 +140,14 @@ def contacts(request):
 
 
 def product(request, pk):
-    product = get_object_or_404(Product, pk=pk)
+    product = get_product(pk)
 
-    same_product = get_hot_product().category.product_set.filter(is_active=True).exclude(pk=get_hot_product().pk)
+    same_product = get_hot_product().category.product_set.filter(
+        is_active=True).select_related().exclude(pk=get_hot_product().pk)
 
     context = {
         'page_title': 'каталог',
-        'categories': get_menu(),
+        'categories': get_links_menu(),
         'category': product.category,
         'product': product,
         'hot_product': get_hot_product,
